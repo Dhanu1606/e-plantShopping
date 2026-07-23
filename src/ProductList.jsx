3d3ec8808a95b15a2b534ea486189af617adf8c6
Dhanu1606 <dhanu675.s@gmail.com>
2026-07-23 09:42:00 -0400

    Added add to cart logic in CartSlice.jsx file

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -219,12 +219,12 @@ function ProductList({ onHomeClick }) {
     ];
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
-      
+
         setAddedToCart((prevState) => ({
-          ...prevState,
-          [product.name]: true,
+            ...prevState,
+            [product.name]: true,
         }));
-      };
+    };
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -288,46 +288,43 @@ function ProductList({ onHomeClick }) {
             {!showCart ? (
                 <div className="product-grid">
                     <div className="product-grid">
-  {plantsArray.map((category, index) => (
-    <div key={index}>
-      <h1>{category.category}</h1>
+                        {plantsArray.map((category, index) => (
+                            <div key={index}>
+                                <h1>{category.category}</h1>
 
-      <div className="product-list">
-        {category.plants.map((plant, plantIndex) => (
-          <div className="product-card" key={plantIndex}>
-            <img
-              className="product-image"
-              src={plant.image}
-              alt={plant.name}
-            />
+                                <div className="product-list">
+                                    {category.plants.map((plant, plantIndex) => (
+                                        <div className="product-card" key={plantIndex}>
+                                            <img
+                                                className="product-image"
+                                                src={plant.image}
+                                                alt={plant.name}
+                                            />
 
-            <div className="product-title">
-              {plant.name}
-            </div>
+                                            <div className="product-title">
+                                                {plant.name}
+                                            </div>
 
-            <div className="product-description">
-              {plant.description}
-            </div>
+                                            <div className="product-description">
+                                                {plant.description}
+                                            </div>
 
-            <div className="product-cost">
-              {plant.cost}
-            </div>
+                                            <div className="product-cost">
+                                                {plant.cost}
+                                            </div>
 
-            <button
-              className="product-button"
-              onClick={() => handleAddToCart(plant)}
-              disabled={addedToCart[plant.name]}
-            >
-              {addedToCart[plant.name]
-                ? "Added to Cart"
-                : "Add to Cart"}
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  ))}
-</div>
+                                            <button
+                                                className="product-button"
+                                                onClick={() => handleAddToCart(plant)}
+                                            >
+                                                {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 
                 </div>
             ) : (
